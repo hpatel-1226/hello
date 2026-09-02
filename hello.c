56a91ad4feb6c580a9e08669c62d72a767c6ab69
Heer Patel <hpatel12@huskers.unl.edu>
2026-09-02 15:37:34 -0500

    added my major to the ouput

--- hello.c
+++ hello.c
@@ -2,10 +2,12 @@
  * Basic Hello World Program
  */
 
+#include <stdio.h>
+
 int main(int argc, char **argv) {
 
-    printf("Hello, I'm Heer Patel!\n");
-    printf("Major is Computer Enginnering!\n");
+    printf("Hello, I'm Heer Patel!!!!\n");
+    printf("My major is Computer Enginnering\n");
 
     return 0;
 }
