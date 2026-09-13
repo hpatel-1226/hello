8aa0ef54b3a3478d3fa52aea25aa16984d8ccdc6
hpatel-1226 <hpatel12@huskers.unl.edu>
2026-09-13 20:28:18 +0000

    Sun, Sep 13, 2026, 3:28 PM -05:00

--- dataPlan.c
+++ dataPlan.c
@@ -56,7 +56,7 @@
         printf("You are right on your average daily use .3f GB/day\n", allowed_average);
       }
       double safe_remaining_average = remaining_GB / remaining_day;
-      printf("To stay under your plan, you can use up to ");
+      printf("To stay under your plan, you can use up to %.3f GB/day for the rest of the 30 day period\n", safe_remaining);
     }
 
 
